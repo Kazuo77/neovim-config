6ac89c7796b5c2c66be4c3d31c64a49525f11d6c
Mathew <kazuo77@gmail.com>
2026-01-28 02:23:32 -0500

    python debugging added

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,22 +1,30 @@
 return {
   {
-    "hrsh7th/nvim-cmp",
+    --nvim-cmp
+    "L3MON4D3/LuaSnip",
     dependencies = {
+      "hrsh7th/nvim-cmp",
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-cmdline",
       "L3MON4D3/LuaSnip",
       "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets", -- vscode style snippits
     },
     config = function()
       local cmp = require("cmp")
+      require("luasnip.loaders.from_vscode").lazy_load()
 
       cmp.setup({
         snippet = {
           expand = function(args)
             require("luasnip").lsp_expand(args.body)
           end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
         },
         mapping = cmp.mapping.preset.insert({
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
@@ -37,10 +45,10 @@ return {
       cmp.setup.cmdline(":", {
         mapping = cmp.mapping.preset.cmdline(),
         sources = cmp.config.sources({
-          { name = "path" }
+          { name = "path" },
         }, {
-          { name = "cmdline" }
-        })
+          { name = "cmdline" },
+        }),
       })
     end,
   },
