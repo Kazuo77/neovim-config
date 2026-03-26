ed6f0216b8ded031e4a3777f11e05f7312041970
Mathew <kazuo77@gmail.com>
2026-03-26 00:33:14 -0400

    fixed clipboard for legacy mac, added Space + sv/sc/so for split creation added ctrl + h/j/k/l for split navigation

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,4 +6,15 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.opt.number = true         -- Show absolute line number on current line
 --vim.opt.relativenumber = true -- Show relative line numbers on other lines
+vim.opt.clipboard = "unnamedplus" 
 vim.opt.mouse = "a" --enable mouse
+
+--splits--
+vim.keymap.set("n", "<leader>sv", ":vsplit<CR>")
+vim.keymap.set("n", "<leader>sh", ":split<CR>")
+vim.keymap.set("n", "<leader>so", ":only<CR>")
+vim.keymap.set("n", "<leader>sc", ":close<CR>")
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
