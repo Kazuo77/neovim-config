a3d659cc7f84af467e9444c1f77b786b0a0cefff
Mathew <kazuo77@gmail.com>
2026-04-06 09:30:04 -0400

    fixed copy paste in nvim for Iterm on OSX 11.7.10

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,6 +6,10 @@ return {
       auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
-    }
+    },
+    config = function(_, opts)
+      vim.opt.runtimepath:append(vim.fn.stdpath("data") .. "/site")
+      require("nvim-treesitter").setup(opts)
+    end,
   }
 }
